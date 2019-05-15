d7284b9234f219049951586c2b41f1061bea844e
Pawel Matusiak <pawel.matusiak29@gmail.com>
2019-05-15 23:17:21 +0200

    refactor

--- src/main/java/pl/ml/Main.java
+++ src/main/java/pl/ml/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +24,8 @@ public class Main extends Application {
         fxmlLoader.load();
 
         Parent root = fxmlLoader.getRoot();
+        primaryStage.getIcons().add(new Image("question-mark-square-01.png"));
+        primaryStage.setTitle("Hi-Lo Game");
         GuessingGame game = fxmlLoader.getController();
         game.setStage(primaryStage);
         game.newGame();
